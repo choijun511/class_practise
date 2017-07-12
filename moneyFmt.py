63b1bf99a1ddb3b2991363ef2ec41c2d5d6884ce
choijun511 <choijun@163.com>
2017-07-12 20:28:51 +0800

    multiprocessing
    我用ipython一行一行运行是可以的，但为什么直接python执行会报错：ImportError: cannot import name
    'Process'

---
+++ moneyFmt.py
@@ -0,0 +1,17 @@
+class MoneyFmt(object):
+    '''数字转换成货币的类，宝典13.18的练习3'''
+
+    def __init__(self, money):
+        self.Money = money
+
+    def update(new_money):
+        self.Money = new_money
+
+    def __nonzero__():
+        pass
+
+    def __repr__():
+        pass
+
+    def __str__():
+        pass
